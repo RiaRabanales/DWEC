f87eb2e78897c8870900b330006b02e1c31525a9
RiaRabanales <mrabanales@cifpfbmoll.eu>
2020-10-07 17:31:49 +0200

    {10oct} fin de clase 2

--- ec01/ec01.js
+++ ec01/ec01.js
@@ -1,12 +1,19 @@
+var myInput;
+
+/**
+ * Función reutilizable en todas las demás: toma el input, lo imprime por pantalla y lo devuelve.
+ */
 function captureInput(){
     myInput = document.getElementById("myInput").value;
     console.log("Input: " + myInput);
     return myInput;
 }
 
+/**
+ * Función del ejercicio 01: numero signe.
+ */
 function numSign(){
     myInput = captureInput();
-    //TODO: comprobar que sea número y que no entre¿???
 
     if (myInput == 0){
         document.getElementById("divResults").innerHTML = "ZERO";
@@ -18,3 +25,93 @@ function numSign(){
         document.getElementById("divResults").innerHTML = "WRONG INPUT VALUE";
     }
 }
+
+/**
+ * Función del ejercicio 02: es numero.
+ */
+function isNumber(){
+    myInput = captureInput();
+    //TODO: comprobar que no me pase algo vacío¿???
+
+    if (isNaN(myInput)) {
+        document.getElementById("divResults").innerHTML = myInput + " IS NOT A NUMBER.";
+        //También podría usar: !number(myInput)
+    } else {
+        document.getElementById("divResults").innerHTML = myInput + " IS A NUMBER.";
+    }
+}
+
+/**
+ * Función del ejercicio 03: numero part entera.
+ */
+//TODO
+
+/**
+ * Función del ejercicio 04: numero part decimals.
+ */
+//TODO
+
+/**
+ * Función del ejercicio 05.
+ */
+//TODO
+
+/**
+ * Función del ejercicio 06.
+ */
+//TODO
+
+/**
+ * Función del ejercicio 07.
+ */
+//TODO
+
+/**
+ * Función del ejercicio 08.
+ */
+//TODO
+
+/**
+ * Función del ejercicio 09.
+ */
+//TODO
+
+/**
+ * Función del ejercicio 10.
+ */
+//TODO
+
+/**
+ * Función del ejercicio 11.
+ */
+//TODO
+
+/**
+ * Función del ejercicio 12.
+ */
+//TODO
+
+/**
+ * Función del ejercicio 13.
+ */
+//TODO
+
+/**
+ * Función del ejercicio 14.
+ */
+//TODO
+
+/**
+ * Función del ejercicio 15.
+ */
+//TODO
+
+/**
+ * Función del ejercicio 16.
+ */
+//TODO
+
+/**
+ * Función del ejercicio 17.
+ */
+//TODO
